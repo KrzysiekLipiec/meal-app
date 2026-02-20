a6d84a2ce5b9809168ec9ae204292e5a7d4ad6e7
Techpriest <krzysztof.kacper.lipiec@gmail.com>
2026-02-20 09:23:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Meal app
+
+TBD
+
+## Tech Stack
+
+- React
+- TypeScript
+- React Compiler
+- rolldown-vite
+- pnpm
+
+## Commands
+
+```
+pnpm install - Install dependencies
+pnpm dev - Start development server
+pnpm build - Build for production
+```
---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      // Enable lint rules for React
+      reactX.configs["recommended-typescript"],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
+      prettierConfig,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      indent: ["error", 2],
+    },
+  },
+]);
